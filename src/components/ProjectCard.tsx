b51e43a36076df08a084bc6255b37140131b8149
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2025-01-05 15:40:32 -0300

    fix(react): 👔 whole card is a link to website

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,41 +17,48 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className="bg-gray-900 border border-gray-700 shadow hover:shadow-xl transition-transform duration-100 hover:scale-105 overflow-hidden flex flex-col">
-      {imagem && (
-        <img
-          src={imagem}
-          alt={`Captura de tela do projeto ${nome}, mostrando a interface principal.`}
-          className="w-full h-40 object-cover"
-        />
-      )}
-      <div className="p-4 flex flex-col flex-grow">
-        <h2 className="text-lg font-extrabold text-white">{nome}</h2>
-        <p className="text-green-300 mt-2 leading-relaxed font-normal">
-          {descricao}
-        </p>
-        <div className="mt-auto flex space-x-4 pt-4">
-          <a
-            href={linkSite}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-green-100 hover:text-green-200 hover:underline font-medium"
-            aria-label={`Visitar o site do projeto ${nome}`}
-          >
-            Visitar Site
-          </a>
-          {linkCodigo && (
+      <a
+        href={linkSite}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label={`Visitar o site do projeto ${nome}`}
+      >
+        {imagem && (
+          <img
+            src={imagem}
+            alt={`Captura de tela do projeto ${nome}, mostrando a interface principal.`}
+            className="w-full h-40 object-cover"
+          />
+        )}
+        <div className="p-4 flex flex-col flex-grow">
+          <h2 className="text-lg font-extrabold text-white">{nome}</h2>
+          <p className="text-green-300 mt-2 leading-relaxed font-normal">
+            {descricao}
+          </p>
+          <div className="flex flex-row space-x-4 pt-4">
             <a
-              href={linkCodigo}
+              href={linkSite}
               target="_blank"
               rel="noopener noreferrer"
               className="text-green-100 hover:text-green-200 hover:underline font-medium"
-              aria-label={`Acessar o código fonte do projeto ${nome} no GitHub`}
+              aria-label={`Visitar o site do projeto ${nome}`}
             >
-              Código no GitHub
+              Visitar Site
             </a>
-          )}
+            {linkCodigo && (
+              <a
+                href={linkCodigo}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-green-100 hover:text-green-200 hover:underline font-medium"
+                aria-label={`Acessar o código fonte do projeto ${nome} no GitHub`}
+              >
+                Código no GitHub
+              </a>
+            )}
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
